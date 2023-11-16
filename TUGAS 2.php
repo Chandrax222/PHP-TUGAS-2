d7cf1fd9d71bb04fecba5cd5bb38693dae92dee1
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-16 23:59:33 +0700

    Menghitung penghasilan kerja
    Menghitung penghasilan kerja

---
+++ TUGAS 2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2</title>
+</head>
+<body>
+            <?php
+                $jual = 80000;
+                $beli = 50000;
+
+                echo ("Jual = 80000");
+                echo "<br>";
+                echo ("Beli = 50000");
+                echo "<br>";
+                echo ("Keuntungan = ?");
+                print "<br>";
+                $keuntungan = $jual % $beli;
+                print "Jawaban : $keuntungan";
+                echo "<hr>"
+                    
+               ?>
+
+
+</body>
+</html>
